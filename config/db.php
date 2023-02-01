05e3eb503e9227a52a9bc646622e29a9070329ea
kpzaolod6000 <43651853+kpzaolod6000@users.noreply.github.com>
2023-02-01 09:22:10 -0500

    config db

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=172.30.4.21;dbname=shcl',
+    'username' => 'ce',
+    'password' => '$erverHQ2021',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
