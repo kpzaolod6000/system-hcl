4458e121fb0d3ce363c49b0fe83ea885130fd6d7
kpzaolod6000 <43651853+kpzaolod6000@users.noreply.github.com>
2023-03-13 10:56:39 -0500

    fix login

--- views/site/login.php
+++ views/site/login.php
@@ -7,21 +7,26 @@
 use yii\bootstrap5\ActiveForm;
 use yii\bootstrap5\Html;
 
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
+
+// $this->title = 'Hospital Quillabamba';
+// $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="d-flex flex-column align-items-center">
+        <!-- <h1><?= Html::encode($this->title) ?></h1> -->
+        <h1>Hospital</h1>
+        <h1>Quillabamba</h1>
 
-    <p>Please fill out the following fields to login:</p>
-
+        <h3>Sistema de Cupos</h3>
+        <p>Por favor, complete los siguientes campos para iniciar sesion</p>
+    </div>
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
         'layout' => 'horizontal',
         'fieldConfig' => [
             'template' => "{label}\n{input}\n{error}",
-            'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
-            'inputOptions' => ['class' => 'col-lg-3 form-control'],
+            'labelOptions' => ['class' => 'col-lg-3 col-form-label mr-lg-3'],
+            'inputOptions' => ['class' => 'col-lg-6 form-control'],
             'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
         ],
     ]); ?>
@@ -31,12 +36,12 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= $form->field($model, 'password')->passwordInput() ?>
 
         <?= $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"offset-lg-1 col-lg-3 custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+            'template' => "<div class=\"col-lg-5 custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
         ]) ?>
 
-        <div class="form-group">
-            <div class="offset-lg-1 col-lg-11">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+        <div class="fom-group">
+            <div class="offset-lg-7 col-lg-1">
+                <?= Html::submitButton('Ingresar', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
             </div>
         </div>
 
