d2b7306b2c5cd27f4505ca1246273a03abaabdbe
Bitler <bitlerarriola@gmail.com>
2023-03-21 18:56:26 -0500

    update view and layout login

--- views/site/login.php
+++ views/site/login.php
@@ -1,54 +1,59 @@
 <?php
 
-/** @var yii\web\View $this */
-/** @var yii\bootstrap5\ActiveForm $form */
-/** @var app\models\LoginForm $model */
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\LoginForm */
 
+use yii\helpers\Html;
 use yii\bootstrap5\ActiveForm;
-use yii\bootstrap5\Html;
 
-
-// $this->title = 'Hospital Quillabamba';
-// $this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <div class="d-flex flex-column align-items-center">
-        <!-- <h1><?= Html::encode($this->title) ?></h1> -->
-        <h1>Hospital</h1>
-        <h1>Quillabamba</h1>
-
-        <h3>Sistema de Cupos</h3>
-        <p>Por favor, complete los siguientes campos para iniciar sesion</p>
+<div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="../../index2.html" class="h1"><b>Historia Clínica</b><br>ue410 <b>HQ</b></a>
     </div>
-    <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'layout' => 'horizontal',
-        'fieldConfig' => [
-            'template' => "{label}\n{input}\n{error}",
-            'labelOptions' => ['class' => 'col-lg-3 col-form-label mr-lg-3'],
-            'inputOptions' => ['class' => 'col-lg-6 form-control'],
-            'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
-        ],
-    ]); ?>
+    <div class="card-body">
+      <p class="login-box-msg">Incie sesión con sus credenciales</p>
 
-        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-        <?= $form->field($model, 'password')->passwordInput() ?>
-
-        <?= $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"col-lg-5 custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-        ]) ?>
-
-        <div class="fom-group">
-            <div class="offset-lg-7 col-lg-1">
-                <?= Html::submitButton('Ingresar', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+      <?php $form = \yii\bootstrap4\ActiveForm::begin(['id' => 'login-form']) ?>
+      <?= $form->field($model,'username', [
+            'options' => ['class' => 'form-group has-feedback'],
+            'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-envelope"></span></div></div>',
+            'template' => '{beginWrapper}{input}{error}{endWrapper}',
+            'wrapperOptions' => ['class' => 'input-group mb-3']
+        ])
+            ->label(false)
+            ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
+        <?= $form->field($model, 'password', [
+            'options' => ['class' => 'form-group has-feedback'],
+            'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-lock"></span></div></div>',
+            'template' => '{beginWrapper}{input}{error}{endWrapper}',
+            'wrapperOptions' => ['class' => 'input-group mb-3']
+        ])
+            ->label(false)
+            ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+            <?= $form->field($model, 'rememberMe')->checkbox([
+                    'template' => '<div class="icheck-primary">{input}{label}</div>',
+                    'labelOptions' => [
+                        'class' => ''
+                    ],
+                    'uncheck' => null
+                ]) ?>
             </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
         </div>
 
-    <?php ActiveForm::end(); ?>
-
-    <!-- <div class="offset-lg-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-    </div> -->
-</div>
+        <?php \yii\bootstrap4\ActiveForm::end(); ?>
+    </div>
+    <!-- /.card-body -->
+  </div>
